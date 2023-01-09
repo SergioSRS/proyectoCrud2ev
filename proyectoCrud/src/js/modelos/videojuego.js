f7c223d75bc3012be73905adc627c917f20af347
Sergio <sergioriverasalgado15@gmail.com>
2023-01-09 11:00:24 +0100

    nueva estructura con el principio de la clase videojuego

---
+++ proyectoCrud/src/js/modelos/videojuego.js
@@ -0,0 +1,10 @@
+class Videojuego{
+    constructor(cita, precio, fecha, descripcion, disponibilidad, edadRecomendada ){
+        this.cita=cita;
+        this.precio=precio;
+        this.fecha=fecha;
+        this.descripcion=descripcion;
+        this.disponibilidad=disponibilidad;
+        this.edadRecomendada = edadRecomendada;
+    }
+}
