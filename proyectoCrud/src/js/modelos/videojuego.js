bfe1fdf994eccfdd6486faaacbcc29d5de5ae089
Sergio <sergioriverasalgado15@gmail.com>
2023-01-10 09:25:33 +0100

    cambios en la clase videojuego

--- proyectoCrud/src/js/modelos/videojuego.js
+++ proyectoCrud/src/js/modelos/videojuego.js
@@ -1,10 +1,20 @@
 class Videojuego{
-    constructor(cita, precio, fecha, descripcion, disponibilidad, edadRecomendada ){
-        this.cita=cita;
-        this.precio=precio;
-        this.fecha=fecha;
-        this.descripcion=descripcion;
-        this.disponibilidad=disponibilidad;
+    constructor(nombre, precio, fecha, descripcion, estado, edadRecomendada, tematicas, imagen){
+        //texto corto
+        this.nombre = nombre;
+        //numero decimal
+        this.precio = precio;
+        //fecha, sin hora
+        this.fecha = fecha;
+        //texto largo
+        this.descripcion = descripcion;
+        //Boolean
+        this.estado = estado;
+        //Atributo seleccionado
         this.edadRecomendada = edadRecomendada;
+        //Atributo multivaluado
+        this.tematicas = tematicas;
+        //tipo blob
+        this.imagen = imagen;
     }
 }
